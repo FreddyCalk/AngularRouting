35a8257d37bd9db69664896a65a840384b4d5995
FreddyCalk <freddycalk@gmail.com>
2015-11-17 11:52:22 -0500

    using routing
    extract template into HTML files

--- js/controller.js
+++ js/controller.js
@@ -1,10 +1,25 @@
-var countryApp = angular.module('countryApp',[]);
+var countryApp = angular.module('countryApp',['ngRoute']);
 
-countryApp.controller('countryController',function($scope, $http){
+countryApp.config(function($routeProvider){
+    $routeProvider.when('/',{
+        templateUrl: 'country-list.html',
+        controller: 'countryCntrl'        
+    }).
+    when('/:countryName',{
+        templateUrl: 'country-detail.html',
+        controller: 'countryDetailCntrl'
+    }).
+    otherwise({
+        redirectTo: '/'
+    });
+});
 
-	$http.get('object.json').success(function(countryData){
-		console.log(countryData);
-		$scope.countries = countryData;
-	})
+countryApp.controller('countryCntrl', function($scope, $http){
+    $http.get('js/object.json').success(function(countryData){
+        $scope.countries = countryData;
+    });
+});
 
+countryApp.controller('countryDetailCntrl', function($scope, $routeParams){
+    console.log($routeParams);
 });
